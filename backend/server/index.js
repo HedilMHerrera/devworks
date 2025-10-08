556579c010c342c00b7561bf00c86728968c3f3e
Cesar <cesar.j.pardo.barrientos@gmail.com>
2025-10-07 21:22:09 -0400

    fixed first test

--- backend/server/index.js
+++ backend/server/index.js
@@ -38,7 +38,7 @@ app.post('/login',async (req,res) => {
     return res.status(200).json({ message: "Acceso correcto" });
 });*/
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/',(req,res) => {
+    res.send('Bienvenido a PyCraft');
 });
 module.exports= app;

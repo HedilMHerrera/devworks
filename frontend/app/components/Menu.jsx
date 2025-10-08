6708ce49c7fd0037ea8f1c0a120f2334cf83c172
Cesar <cesar.j.pardo.barrientos@gmail.com>
2025-10-08 00:47:56 -0400

    menu added

---
+++ frontend/app/components/Menu.jsx
@@ -0,0 +1,81 @@
+'use client';
+import React,{ useContext, useState, useEffect } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import SnakeIcon from '../snakeicon/SnakeIcon';
+import { useRouter } from 'next/navigation';
+import style from './menu.module.css';
+import { SessionContext } from '../context/SessionContext';
+import UserStudentMenu from './UserStudentMenu';
+export const MenuCustom = () => {
+  const { dataUser } = useContext(SessionContext);
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+  return (
+    <Box>
+      <AppBar position="fixed" color="background">
+        <Toolbar sx={{
+
+            justifyContent: "space-between",
+            alignItems:"center",
+            gap:0
+        }}>
+        <MenuItem sx={{
+          gap:1,
+          display:{
+            xs:'none',
+            sm:'flex'
+          }
+        }} onClick={() => { router.push('/') }}>
+            <SnakeIcon width={30} height={30}/>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                PyCraft
+            </Typography>
+        </MenuItem>
+          <Box
+          display="flex"
+          justifyContent="space-between"
+          flexDirection="row"
+          alignItems='center'
+          alignSelf='center'
+          width={{
+            sm:'fit-content',
+            xs:"100%"
+          }}
+          gap={3}
+        >
+            <Button 
+              className={`${style['shrink-border']}`} 
+              color="primary"
+              onClick={() => { router.push('/') }}
+            >Inicio</Button>
+            <Button className={`${style['shrink-border']}`} color="primary">Grupos</Button>
+            {(!dataUser) ? 
+        (<Button 
+              className={`${style['shrink-border']}`} 
+              onClick={() => {router.push('/login')}}
+              sx={{
+                bgcolor:"primary.main",
+                color:"primary.contrastText",
+                '&:hover':{
+                  color:"primary.main",
+                  bgcolor:"transparent",
+                }
+              }}
+              >Login</Button>):<UserStudentMenu />}
+        </Box>
+        
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}

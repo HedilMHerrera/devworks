1410b7fdb8be0d37838c34fb312dcced2a24189d
Cesar <cesar.j.pardo.barrientos@gmail.com>
2025-10-16 19:23:49 -0400

    eslit preconfigured 2

---
+++ backend/test/app.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../server/index.js");
+
+describe("GET /", () => {
+  it("respuesta esperada: Bienvenido a Pycraft", async() => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Bienvenido a PyCraft");
+  });
+});

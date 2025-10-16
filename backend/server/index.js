96fbc4cad86635039e0a2604b2da1b0ad250720a
Cesar <cesar.j.pardo.barrientos@gmail.com>
2025-10-16 19:35:22 -0400

    dos

--- backend/server/index.js
+++ backend/server/index.js
@@ -72,8 +72,8 @@ app.post("/logingoogle", async(req, res) => {
       maxAge: 60 * 60 * MAX_TIME,
     });
 
-    const uno = "";
-    console.log(uno);
+    const ddos = "";
+    console.log(ddos);
 
     return res.status(200).json({ token, user });
   } catch (e) {
